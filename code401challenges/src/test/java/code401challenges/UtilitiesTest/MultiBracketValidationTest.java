b82247316f307421313cfb73f4abc3d347d1e344
tcoots88 <travis.coots@yahoo.com>
2020-01-23 13:55:43 -0800

    multiBracketinProgress

--- code401challenges/src/test/java/code401challenges/UtilitiesTest/MultiBracketValidationTest.java
+++ code401challenges/src/test/java/code401challenges/UtilitiesTest/MultiBracketValidationTest.java
@@ -5,10 +5,5 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MultiBracketValidationTest {
-
-    @Test
-    public void testMatchingCurlyBrackets() {
-        MultiBracketValidation multibracketValidation = new MultiBracketValidation();
-        assertTrue(multibracketValidation.multiBracketValidation("{}"));
     }
 }
